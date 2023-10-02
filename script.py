c772c5a7f12e09914b00c7fef8aa14bcad05597a
TurquoiseFiroozeh <145533561+TurquoiseFiroozeh@users.noreply.github.com>
2023-10-02 09:54:46 -0600

    Initial Commit

---
+++ script.py
@@ -0,0 +1,27 @@
+"Script.py is only a sandbox for me to test different scenarios"
+# import math
+import sys
+# from os import rename
+
+import requests
+
+print(f"1:    {sys.version}")
+# print(sys.version)
+print(f"2:    {sys.executable}")
+# print(sys.executable)
+
+
+def greet(who_to_greet):
+    "get a name and return greeting string"
+    # greeting = "Hello, {}".format(who_to_greet)
+    greeting = f"Hello, {who_to_greet}"
+    return greeting
+
+
+name = input("3:    Your Name?")
+print(f"4:    {greet(name)}")
+# print(greet("Masoud"))
+
+r = requests.get(url='https://www.google.com', timeout=None)
+print(f"5:    Request Code = {r.status_code}")
+# print(r.status_code)
